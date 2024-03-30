0240918607d754b6be6033c791f0260100d18104
JulienBrandi <92440475+JulienBrandi@users.noreply.github.com>
2024-03-30 17:53:24 +0100

    first commit

---
+++ src/main/java/org/example/Controleur.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Controleur {
+
+    private final JFrame window;
+
+    public Controleur(){
+        this.window = new JFrame();
+        this.window.setTitle("Zelda 2D");
+        this.window.setSize(1920, 1080);
+        this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void startApp(){
+
+    }
+
+}
